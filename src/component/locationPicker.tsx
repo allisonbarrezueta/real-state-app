f077ec002a222f7f31b1b65fa2111ba8d713de95
Allison Barrezueta <allisonbarrezueta@gmail.com>
2025-05-05 16:08:07 +0200

    first commit

---
+++ src/component/locationPicker.tsx
@@ -0,0 +1,136 @@
+import React, { useRef, useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader, StandaloneSearchBox, Libraries } from "@react-google-maps/api";
+
+const containerStyle = {
+    width: "100%",
+    height: "400px",
+};
+
+const defaultCenter = {
+    lat: 41.3851,
+    lng: 2.1734,
+};
+
+interface PlaceInfo {
+    lat: number;
+    lng: number;
+    city: string;
+    neighborhood: string;
+    district: string;
+}
+
+const LocationPicker: React.FC = () => {
+    const [center, setCenter] = useState(defaultCenter);
+    const [placeInfo, setPlaceInfo] = useState<PlaceInfo | null>(null);
+    const autocompleteRef = useRef<google.maps.places.SearchBox | null>(null);
+    const [libraries] = useState<Libraries>(["drawing", "geometry", "places", "visualization"]);
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: "AIzaSyBKO9OFZ8qypY2eykvm2rVNt-yA0oSDddw", // replace this
+        libraries,
+    });
+
+    const fetchPlaceInfo = async (lat: number, lng: number) => {
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBKO9OFZ8qypY2eykvm2rVNt-yA0oSDddw`);
+        const data = await response.json();
+        console.log({ data });
+        if (data.status === "OK") {
+            console.log("==>", data.results);
+            const components = data.results[0]?.address_components || [];
+
+            const getComponent = (types: string[]): string => components.find((c: any) => types.every((t) => c.types.includes(t)))?.long_name || "";
+
+            setPlaceInfo({
+                lat,
+                lng,
+                city: getComponent(["locality"]),
+                neighborhood: getComponent(["sublocality", "sublocality_level_1"]),
+                district: getComponent(["administrative_area_level_2"]),
+            });
+        }
+    };
+
+    const onPlaceChanged = async () => {
+        if (!autocompleteRef.current) return;
+
+        const place = autocompleteRef.current.getPlaces();
+        if (place && place?.length === 1) {
+            const newPosition = place[0]?.geometry?.location?.toJSON();
+
+            if (newPosition) {
+                setCenter({ lat: newPosition.lat, lng: newPosition.lng });
+                // await fetchPlaceInfo(newPosition.lat, newPosition.lng);
+            }
+        }
+    };
+
+    const handleDoubleClick = async (e: google.maps.MapMouseEvent) => {
+        if (e.latLng) {
+            const lat = e.latLng.lat();
+            const lng = e.latLng.lng();
+            console.log({ lat, lng });
+            setCenter({ lat, lng });
+            await fetchPlaceInfo(lat, lng);
+        }
+    };
+
+    const handleMarkerDragEnd = (event: google.maps.MapMouseEvent) => {
+        if (event.latLng) {
+            const lat = event.latLng.lat();
+            const lng = event.latLng.lng();
+            setCenter({ lat, lng });
+            fetchPlaceInfo(lat, lng);
+        }
+    };
+    console.log({ placeInfo });
+    if (!isLoaded) return <div>Loading map...</div>;
+
+    return (
+        <div className="rounded-xl shadow-lg p-4 bg-white">
+            <StandaloneSearchBox onLoad={(ref) => (autocompleteRef.current = ref)} onPlacesChanged={onPlaceChanged}>
+                <input type="text" placeholder="Search a place" className="w-full p-2 mb-2.5 border border-gray-300 rounded-md" />
+            </StandaloneSearchBox>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={14}
+                onDblClick={handleDoubleClick}
+                onLoad={(map) => {
+                    if (autocompleteRef.current) {
+                        map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(autocompleteRef.current as unknown as HTMLElement);
+                    }
+                }}>
+                <Marker position={center} onDragEnd={handleMarkerDragEnd} />
+            </GoogleMap>
+
+            {placeInfo && (
+                <div style={{ marginTop: "1rem" }}>
+                    <p>
+                        <strong>Latitude:</strong> {placeInfo.lat}
+                    </p>
+                    <p>
+                        <strong>Longitude:</strong> {placeInfo.lng}
+                    </p>
+                    <p>
+                        <strong>City:</strong> {placeInfo.city}
+                    </p>
+                    <p>
+                        <strong>Neighborhood:</strong> {placeInfo.neighborhood}
+                    </p>
+                    <p>
+                        <strong>District:</strong> {placeInfo.district}
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default LocationPicker;
+
+//<script async defer
+//   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKO9OFZ8qypY2eykvm2rVNt-yA0oSDddw">
+// </script>
+// GOOGLE MAPS API KEY AIzaSyBKO9OFZ8qypY2eykvm2rVNt-yA0oSDddw
+
+// another api key AIzaSyBKO9OFZ8qypY2eykvm2rVNt-yA0oSDddw
---
+++ src/layout/navbar.tsx
@@ -0,0 +1,89 @@
+import { Dialog, DialogBackdrop, DialogPanel, PopoverGroup } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+
+const navigation = {
+    pages: [
+        { name: "Predictive", href: "" },
+        { name: "Stats", href: "stats" },
+        { name: "Exploratory", href: "explore" },
+    ],
+};
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="bg-white">
+            {/* Mobile menu */}
+            <Dialog open={open} onClose={setOpen} className="relative z-40 lg:hidden">
+                <DialogBackdrop transition className="fixed inset-0 bg-black/25 transition-opacity duration-300 ease-linear data-[closed]:opacity-0" />
+
+                <div className="fixed inset-0 z-40 flex">
+                    <DialogPanel
+                        transition
+                        className="relative flex w-full max-w-xs transform flex-col overflow-y-auto bg-white pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:-translate-x-full">
+                        <div className="flex px-4 pb-2 pt-5">
+                            <button type="button" onClick={() => setOpen(false)} className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400">
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon aria-hidden="true" className="size-6" />
+                            </button>
+                        </div>
+                        <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                            {navigation.pages.map((page) => (
+                                <div key={page.name} className="flow-root">
+                                    <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
+                                        {page.name}
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+
+            <header className="relative">
+                <nav aria-label="Top">
+                    {/* Secondary navigation */}
+                    <div className="bg-white">
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                            <div className="border-b border-gray-200">
+                                <div className="flex h-16 items-center justify-between">
+                                    {/* Logo (md+) */}
+                                    <div className="hidden md:flex md:flex-1 md:items-center">
+                                        <a href="#">
+                                            <span className="sr-only">Your Company</span>
+                                            <img alt="" src="src/assets/EAEBSB_LOGO.png" className="h-8 w-auto" />
+                                        </a>
+                                    </div>
+
+                                    <div className="hidden h-full md:flex">
+                                        {/* Flyout menus */}
+                                        <PopoverGroup className="inset-x-0 bottom-0 px-4">
+                                            <div className="flex h-full justify-center space-x-8">
+                                                {navigation.pages.map((page) => (
+                                                    <a key={page.name} href={page.href} className="flex items-center text-sm font-medium text-amber-700 hover:text-amber-800">
+                                                        {page.name}
+                                                    </a>
+                                                ))}
+                                            </div>
+                                        </PopoverGroup>
+                                    </div>
+
+                                    {/* Mobile menu and search (md-) */}
+                                    <div className="flex flex-1 items-center md:hidden">
+                                        <button type="button" onClick={() => setOpen(true)} className="-ml-2 rounded-md bg-white p-2 text-gray-400">
+                                            <span className="sr-only">Open menu</span>
+                                            <Bars3Icon aria-hidden="true" className="size-6" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+};
+
+export default Navbar;
